233eac2c78d1ea6e298cac6442a528f91bacfede
cuonglvm <cuonglvmphp@gmail.com>
2022-10-21 15:57:28 +0700

    config db

--- signup/config.php
+++ signup/config.php
@@ -1,7 +1,7 @@
 <?php
    define('DB_SERVER', 'localhost');
-   define('DB_USERNAME', 'lifegewe_lgo');
-   define('DB_PASSWORD', 'CYyLt7uJSrw5s@i');
-   define('DB_DATABASE', 'lifegewe_leads');
+   define('DB_USERNAME', 'findejvu_dynuinmedia');
+   define('DB_PASSWORD', 'dynuinmedia@2022');
+   define('DB_DATABASE', 'findejvu_find_financial');
    $conn = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
